ef51322c9cb4e806d1704cc90273fabfc3b4a393
kursad-yildirim <kursad.yildirim@outlook.com>
2021-01-27 00:30:15 +0300

    Motor Class Added.

--- TriP-Bot/lib/tripbot.motor.h
+++ TriP-Bot/lib/tripbot.motor.h
@@ -1,53 +1,72 @@
 // TriP-BoT Motor Related Definitions
 
-int fullMotorSpeed = 123;
-int halfMotorSpeed = 123;
+class tripMotor
+{
+private:
+  int fullMotorSpeed = 123;
+  int halfMotorSpeed = 123;
 
-void setupMotor(){
-  pinMode(motor_A_direcPin, OUTPUT);
-  pinMode(motor_A_brakePin, OUTPUT);
-  pinMode(motor_A_speedPin, OUTPUT);
-  pinMode(motor_B_direcPin, OUTPUT);
-  pinMode(motor_B_brakePin, OUTPUT);
-  pinMode(motor_B_speedPin, OUTPUT);
-}
-void goForward(){
-  digitalWrite(motor_A_direcPin, LOW);
-  digitalWrite(motor_A_brakePin, LOW);
-  digitalWrite(motor_B_direcPin, HIGH);
-  digitalWrite(motor_B_brakePin, LOW);
+public:
+  tripMotor()
+  {
+    // Use 'this->' to make the difference between the
+    // 'pin' attribute of the class and the
+    // local variable 'pin' created from the parameter.
+    //      this->pin = pin;
+    init();
+  }
+  void init()
+  {
+    pinMode(motor_A_direcPin, OUTPUT);
+    pinMode(motor_A_brakePin, OUTPUT);
+    pinMode(motor_A_speedPin, OUTPUT);
+    pinMode(motor_B_direcPin, OUTPUT);
+    pinMode(motor_B_brakePin, OUTPUT);
+    pinMode(motor_B_speedPin, OUTPUT);
+  }
+  void goForward()
+  {
+    digitalWrite(motor_A_direcPin, LOW);
+    digitalWrite(motor_A_brakePin, LOW);
+    digitalWrite(motor_B_direcPin, HIGH);
+    digitalWrite(motor_B_brakePin, LOW);
 
-  analogWrite(motor_A_speedPin, fullMotorSpeed);
-  analogWrite(motor_B_speedPin, fullMotorSpeed);
-}
-void goBackward(){
-  digitalWrite(motor_A_direcPin, HIGH);
-  digitalWrite(motor_A_brakePin, LOW);
-  digitalWrite(motor_B_direcPin, LOW);
-  digitalWrite(motor_B_brakePin, LOW);
+    analogWrite(motor_A_speedPin, fullMotorSpeed);
+    analogWrite(motor_B_speedPin, fullMotorSpeed);
+  }
+  void goBackward()
+  {
+    digitalWrite(motor_A_direcPin, HIGH);
+    digitalWrite(motor_A_brakePin, LOW);
+    digitalWrite(motor_B_direcPin, LOW);
+    digitalWrite(motor_B_brakePin, LOW);
 
-  analogWrite(motor_A_speedPin, fullMotorSpeed);
-  analogWrite(motor_B_speedPin, fullMotorSpeed);
-}
-void turnRight(){
-  digitalWrite(motor_A_direcPin, LOW);
-  digitalWrite(motor_A_brakePin, LOW);
-  digitalWrite(motor_B_direcPin, LOW);
-  digitalWrite(motor_B_brakePin, LOW);
+    analogWrite(motor_A_speedPin, fullMotorSpeed);
+    analogWrite(motor_B_speedPin, fullMotorSpeed);
+  }
+  void turnRight()
+  {
+    digitalWrite(motor_A_direcPin, LOW);
+    digitalWrite(motor_A_brakePin, LOW);
+    digitalWrite(motor_B_direcPin, LOW);
+    digitalWrite(motor_B_brakePin, LOW);
 
-  analogWrite(motor_A_speedPin, halfMotorSpeed);
-  analogWrite(motor_B_speedPin, halfMotorSpeed);
-}
-void turnLeft(){
-  digitalWrite(motor_A_direcPin, HIGH);
-  digitalWrite(motor_A_brakePin, LOW);
-  digitalWrite(motor_B_direcPin, HIGH);
-  digitalWrite(motor_B_brakePin, LOW);
+    analogWrite(motor_A_speedPin, halfMotorSpeed);
+    analogWrite(motor_B_speedPin, halfMotorSpeed);
+  }
+  void turnLeft()
+  {
+    digitalWrite(motor_A_direcPin, HIGH);
+    digitalWrite(motor_A_brakePin, LOW);
+    digitalWrite(motor_B_direcPin, HIGH);
+    digitalWrite(motor_B_brakePin, LOW);
 
-  analogWrite(motor_A_speedPin, halfMotorSpeed);
-  analogWrite(motor_B_speedPin, halfMotorSpeed);
-}
-void fullStop(){
-  digitalWrite(motor_A_brakePin, HIGH);
-  digitalWrite(motor_B_brakePin, HIGH);
-}
+    analogWrite(motor_A_speedPin, halfMotorSpeed);
+    analogWrite(motor_B_speedPin, halfMotorSpeed);
+  }
+  void fullStop()
+  {
+    digitalWrite(motor_A_brakePin, HIGH);
+    digitalWrite(motor_B_brakePin, HIGH);
+  }
+};
